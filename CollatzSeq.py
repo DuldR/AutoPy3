8e808a6d7c299b75e939d504a47be9dcb01df46f
DuldR <duldonkulus@gmail.com>
2019-03-06 20:51:24 -0600

    Update to collatz seq. Completed - Needs cleanup with regards to ensuring proper user input.

--- CollatzSeq.py
+++ CollatzSeq.py
@@ -1,3 +1,23 @@
 def collatz(number):
 
-    if
+    if number % 2 == 0:
+        even = number // 2
+        print(even)
+        #Ensure to return even value and not number variabel
+        return even
+
+    else:
+        odd = 3 * number + 1
+        print(odd)
+        return odd
+
+#User input for number to collatz'd
+request = input("Enter a number: ")
+
+# Setting initial iteration point
+iter = collatz(int(request))
+
+# Iterate over the given value to it reaches 1
+while iter != 1:
+    iter = collatz(iter)
+
